be55a15443e7a2afb458d2ba57e7ce8f9da78602
hjperschy <46538642+hjperschy@users.noreply.github.com>
2024-12-30 22:49:14 +0000

    Corrected correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -151,7 +151,7 @@ def predict(model_type_chat, user_input, model):
         
         model_type_chat = st.session_state.messenger_bot_chat
         
-    response = client.chat.completions.create(
+    response = openai.chat.completions.create(
         model=model,
         messages=model_type_chat,
         temperature = .1)
